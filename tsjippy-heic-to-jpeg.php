5b547853ca175773d31ead738ed14f64585e895b
Tsjippy <ewaldha@hotmail.com>
2026-07-03 18:59:12 +0200

    fixed: plugin settings key

--- tsjippy-heic-to-jpeg.php
+++ tsjippy-heic-to-jpeg.php
@@ -30,7 +30,7 @@ if (! defined('ABSPATH')) {
 define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
 define(__NAMESPACE__ . '\PLUGINPATH', __DIR__ . '/');
 define(__NAMESPACE__ . '\PLUGINVERSION', get_plugin_data(__FILE__, false, false)['Version']);
-define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_heictojpeg_settings', []));
+define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_heic-to-jpeg_settings', []));
 
 // Load shared code
 if(file_exists(__DIR__  . '/shared-functionality/loader.php')){
