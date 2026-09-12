66f4c21a93ef2a26bdf64001cfd0d80dc408ec61
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:26:39 +0200

    changed: removed 'tested up to'

--- tsjippy-heic-to-jpeg.php
+++ tsjippy-heic-to-jpeg.php
@@ -12,7 +12,6 @@ use TSJIPPY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
  * Plugin URI:            https://github.com/Tsjippy/heictojpeg/
  * Tested:               7.1
  * TextDomain:            tsjippy
